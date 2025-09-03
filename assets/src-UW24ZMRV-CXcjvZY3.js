7fb56d7bacbec69d9e4a6b6f2de9cb0ae6e5267d
verstraete-jonatan <138657778+verstraete-jonatan@users.noreply.github.com>
2025-09-03 18:41:55 +0200

    Updates

--- assets/src-UW24ZMRV-CXcjvZY3.js
+++
@@ -1,1 +0,0 @@
-import{a4 as a}from"./features-animation-ClaZ9uVy.js";import"./index-BcUM4Biw.js";var t=a;export{t as default};
---
+++ assets/src-UW24ZMRV-Y4rbzjXp.js
@@ -0,0 +1,1 @@
+import{a4 as a}from"./features-animation-WV_f7MXx.js";import"./index-CoGM8bxq.js";var t=a;export{t as default};
